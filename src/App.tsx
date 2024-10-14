061d941a37c041697ab6195aba1ee898ca097903
w00jinLee <122732558+w00jinLee@users.noreply.github.com>
2024-10-14 11:38:45 +0900

    Revert "헤더 네브 라우터 추가"

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AppRouter from "./Router";
-
 import HomePage from "./pages/home/HomePage";
 
 function App() {
-  // 유저 recoil 세팅 해야함
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<HomePage />}></Route>
-    //   </Routes>
-    // </BrowserRouter>
-    <>
-      <AppRouter />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
