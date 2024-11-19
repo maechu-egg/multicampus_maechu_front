e16262787e61f39336ace244636e7cd1c20e9514
leeyeonju02 <85239317+leeyeonju02@users.noreply.github.com>
2024-11-20 00:04:24 +0900

    feat : 마이페이지 탈퇴

--- src/pages/login/SignPage.tsx
+++ src/pages/login/SignPage.tsx
@@ -6,29 +6,31 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
 function SignPage(): JSX.Element {
-  const [member_img, setMemberImg] = useState<File | null>(null); // 프로필 이미지 상태
+  const [member_img, setMemberImg] = useState<File | null>(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [nickname, setNickname] = useState("");
-  const [phone, setPhone] = useState(""); // 전화번호 상태
+  const [phone, setPhone] = useState("");
   const [emailError, setEmailError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
   const [confirmPasswordError, setConfirmPasswordError] = useState(false);
   const [certificationCode, setCertificationCode] = useState("");
-  const [isEmailValid, setIsEmailValid] = useState(false); // 이메일 유효성 상태 추가
-  const [checkButtonDisabled, setCheckButtonDisabled] = useState(true); // Check 버튼 비활성화 상태
-  const [emailCheckError, setEmailCheckError] = useState(""); // 이메일 중복 체크 에러 메시지
-  const [verificationError, setVerificationError] = useState(""); // 인증 코드 확인 에러 메시지
-  const [nicknameCheckError, setNicknameCheckError] = useState(""); // 닉네임 중복 체크 에러 메시지
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [checkButtonDisabled, setCheckButtonDisabled] = useState(true);
+  const [emailCheckError, setEmailCheckError] = useState("");
+  const [verificationError, setVerificationError] = useState("");
+  const [nicknameCheckError, setNicknameCheckError] = useState("");
   const [isCertificationCodeValid, setIsCertificationCodeValid] =
-    useState(false); // 인증 코드 유효성 상태
-  const [isNicknameValid, setIsNicknameValid] = useState(false); // 닉네임 유효성 상태
-  const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수 생성
-  const { dispatch, state } = useAuth(); // AuthContext에서 dispatch 함수와 state 가져오기
+    useState(false);
+  const [isNicknameValid, setIsNicknameValid] = useState(false);
+  const [isVerificationButtonDisabled, setIsVerificationButtonDisabled] =
+    useState(false);
+  const navigate = useNavigate();
+  const { dispatch, state } = useAuth();
 
   const validateEmail = (email: string) => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 이메일 정규 표현식
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailPattern.test(email);
   };
 
@@ -36,10 +38,14 @@ function SignPage(): JSX.Element {
     const value = e.target.value;
     setEmail(value);
     const isValid = validateEmail(value);
-    setEmailError(!isValid); // 이메일 유효성 검사
-    setIsEmailValid(isValid); // 이메일 유효성 상태 업데이트
-    setCheckButtonDisabled(!isValid); // 이메일이 유효하지 않으면 Check 버튼 비활성화
-    setEmailCheckError(""); // 이메일 체크 에러 메시지 초기화
+    setEmailError(!isValid);
+    setIsEmailValid(isValid);
+    setCheckButtonDisabled(!isValid);
+    setEmailCheckError("");
+
+    if (isVerificationButtonDisabled) {
+      setIsVerificationButtonDisabled(false);
+    }
   };
 
   const handleCheckEmail = async () => {
@@ -48,24 +54,21 @@ function SignPage(): JSX.Element {
         email,
       });
       if (response.status === 200) {
-        // 이메일 사용 가능
-        setCheckButtonDisabled(false); // Check 버튼 활성화
-        setEmailCheckError("인증코드 발송"); // 인증 코드 발송 메시지 설정
-        setIsEmailValid(true); // 이메일 유효성 상태 업데이트
+        setCheckButtonDisabled(false);
+        setEmailCheckError("인증코드 발송");
+        setIsEmailValid(true);
 
-        // 인증 코드 전송 요청
         await api.post("/user/register/email-certification", {
           email,
         });
         console.log("인증 코드가 발송되었습니다.");
       }
     } catch (error) {
-      const axiosError = error as AxiosError; // error를 AxiosError로 캐스팅합니다.
+      const axiosError = error as AxiosError;
       if (axiosError.response && axiosError.response.status === 409) {
-        // 중복된 이메일
-        setCheckButtonDisabled(true); // Check 버튼 비활성화
-        setEmailCheckError("중복된 이메일입니다."); // 에러 메시지 설정
-        setIsEmailValid(false); // 이메일 유효성 상태 업데이트
+        setCheckButtonDisabled(true);
+        setEmailCheckError("중복된 이메일입니다.");
+        setIsEmailValid(false);
       } else {
         console.error("이메일 체크 오류:", error);
       }
@@ -75,7 +78,7 @@ function SignPage(): JSX.Element {
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setPassword(value);
-    setPasswordError(value.length < 6); // 비밀번호 길이 검사
+    setPasswordError(value.length < 6);
   };
 
   const handleConfirmPasswordChange = (
@@ -83,14 +86,14 @@ function SignPage(): JSX.Element {
   ) => {
     const value = e.target.value;
     setConfirmPassword(value);
-    setConfirmPasswordError(value !== password); // 비밀번호 확인 일치 검사
+    setConfirmPasswordError(value !== password);
   };
 
   const handleCertificationCodeChange = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setCertificationCode(e.target.value); // 인증 코드 입력 처리
-    setIsCertificationCodeValid(false); // 인증 코드 유효성 초기화
+    setCertificationCode(e.target.value);
+    setIsCertificationCodeValid(false);
   };
 
   const handleVerifyCode = async () => {
@@ -100,17 +103,16 @@ function SignPage(): JSX.Element {
         certificationCode,
       });
       if (response.status === 200) {
-        // 인증 성공
         console.log("인증 코드가 확인되었습니다.");
-        setIsCertificationCodeValid(true); // 인증 코드 유효성 상태 업데이트
-        setVerificationError(""); // 에러 메시지 초기화
+        setIsCertificationCodeValid(true);
+        setVerificationError("");
+        setIsVerificationButtonDisabled(true);
       }
     } catch (error) {
-      const axiosError = error as AxiosError; // error를 AxiosError로 캐스팅합니다.
+      const axiosError = error as AxiosError;
       if (axiosError.response && axiosError.response.status === 400) {
-        // 인증 실패
-        setIsCertificationCodeValid(false); // 인증 코드 유효성 상태 업데이트
-        setVerificationError("인증 코드가 유효하지 않습니다."); // 에러 메시지 설정
+        setIsCertificationCodeValid(false);
+        setVerificationError("인증 코드가 유효하지 않습니다.");
       } else {
         console.error("인증 코드 확인 오류:", error);
       }
@@ -120,8 +122,8 @@ function SignPage(): JSX.Element {
   const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setNickname(value);
-    setNicknameCheckError(""); // 닉네임 체크 에러 메시지 초기화
-    setIsNicknameValid(false); // 닉네임 유효성 초기화
+    setNicknameCheckError("");
+    setIsNicknameValid(false);
   };
 
   const handleCheckNickname = async () => {
@@ -130,17 +132,15 @@ function SignPage(): JSX.Element {
         nickname,
       });
       if (response.status === 200) {
-        // 닉네임 사용 가능
-        setNicknameCheckError(""); // 에러 메시지 초기화
-        setIsNicknameValid(true); // 닉네임 유효성 상태 업데이트
+        setNicknameCheckError("");
+        setIsNicknameValid(true);
         console.log("닉네임 사용 가능");
       }
     } catch (error) {
-      const axiosError = error as AxiosError; // error를 AxiosError로 캐스팅합니다.
+      const axiosError = error as AxiosError;
       if (axiosError.response && axiosError.response.status === 409) {
-        // 중복된 닉네임
-        setNicknameCheckError("중복된 닉네임입니다."); // 에러 메시지 설정
-        setIsNicknameValid(false); // 닉네임 유효성 상태 업데이트
+        setNicknameCheckError("중복된 닉네임입니다.");
+        setIsNicknameValid(false);
       } else {
         console.error("닉네임 체크 오류:", error);
       }
@@ -150,51 +150,38 @@ function SignPage(): JSX.Element {
   const handleSignUp = async () => {
     try {
       const formData = new FormData();
-
-      // member_img가 null이 아닐 때만 추가
       if (member_img) {
-        formData.append("memberImgFile", member_img); // 프로필 이미지
+        formData.append("memberImgFile", member_img);
       }
+      formData.append("nickname", nickname);
+      formData.append("password", password);
+      formData.append("email", email);
+      formData.append("phone", phone);
 
-      formData.append("nickname", nickname); // 닉네임
-      formData.append("password", password); // 비밀번호
-      formData.append("email", email); // 이메일
-      formData.append("phone", phone); // 전화번호
-
-      // FormData의 내용을 출력
-      for (let [key, value] of formData.entries()) {
-        console.log(`${key}:`, value);
-      }
-
-      // API 요청
       const response = await api.post("/user/register", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
 
-      // 응답 처리
       if (response.status === 200) {
         console.log("회원가입 성공:", response.data);
         const loginResponse = await api.post("/user/login", {
           email,
           password,
         });
-        console.log("서버 응답:", loginResponse.data); // 서버 응답 확인
-        const { token, memberId } = loginResponse.data; // 서버에서 받은 token과 memberId
+        console.log("서버 응답:", loginResponse.data);
+        const { token, memberId } = loginResponse.data;
 
         if (token) {
-          alert("로그인 성공!"); // 로그인 성공 알림
-          localStorage.setItem("authToken", token); // 로컬 스토리지에 토큰 저장
-          localStorage.setItem("memberId", memberId.toString()); // 로컬 스토리지에 memberId 저장
-          dispatch({ type: "LOGIN", payload: { token, memberId } }); // Context에 token과 memberId 저장
-
-          // Context에서 저장된 값을 가져와서 출력
-          console.log("로그인 후 Context 상태:", state); // Context 상태 출력
-
-          navigate("/profile"); // 홈페이지로 이동
+          alert("로그인 성공!");
+          localStorage.setItem("authToken", token);
+          localStorage.setItem("memberId", memberId.toString());
+          dispatch({ type: "LOGIN", payload: { token, memberId } });
+          console.log("로그인 후 Context 상태:", state);
+          navigate("/profile");
         } else {
-          alert("토큰을 받을 수 없습니다."); // 토큰이 없을 경우 알림
+          alert("토큰을 받을 수 없습니다.");
         }
       }
     } catch (error) {
@@ -203,9 +190,17 @@ function SignPage(): JSX.Element {
         "회원가입 오류:",
         axiosError.response ? axiosError.response.data : axiosError.message
       );
-      // 추가적인 오류 처리 로직
     }
   };
+
+  const isSignUpEnabled =
+    isEmailValid &&
+    isNicknameValid &&
+    !passwordError &&
+    !confirmPasswordError &&
+    isCertificationCodeValid &&
+    password === confirmPassword;
+
   return (
     <Container>
       <SignForm>
@@ -219,7 +214,7 @@ function SignPage(): JSX.Element {
               className="form-control"
               type="file"
               id="formFile"
-              onChange={(e) => setMemberImg(e.target.files?.[0] || null)} // 파일 선택 시 상태 업데이트
+              onChange={(e) => setMemberImg(e.target.files?.[0] || null)}
               style={{ width: "100%" }}
             />
           </div>
@@ -242,7 +237,10 @@ function SignPage(): JSX.Element {
                 type="button"
                 className="btn btn-secondary"
                 disabled={checkButtonDisabled}
-                onClick={handleCheckEmail} // 이메일 체크 버튼 클릭 시 핸들러 호출
+                onClick={handleCheckEmail}
+                style={{
+                  backgroundColor: checkButtonDisabled ? "#6C757D" : "#212529",
+                }}
               >
                 Check
               </button>
@@ -274,7 +272,13 @@ function SignPage(): JSX.Element {
               <button
                 type="button"
                 className="btn btn-secondary"
-                onClick={handleVerifyCode} // 인증 코드 확인 버튼 클릭 시 핸들러 호출
+                onClick={handleVerifyCode}
+                disabled={isVerificationButtonDisabled}
+                style={{
+                  backgroundColor: isVerificationButtonDisabled
+                    ? "#6C757D"
+                    : "#212529",
+                }}
               >
                 Verify
               </button>
@@ -305,7 +309,10 @@ function SignPage(): JSX.Element {
               <button
                 type="button"
                 className="btn btn-secondary"
-                onClick={handleCheckNickname} // 닉네임 체크 버튼 클릭 시 핸들러 호출
+                onClick={handleCheckNickname}
+                style={{
+                  backgroundColor: isNicknameValid ? "#A0A5AB" : "#212529",
+                }}
               >
                 Check
               </button>
@@ -369,13 +376,11 @@ function SignPage(): JSX.Element {
               <button
                 type="button"
                 className="btn btn-dark"
-                onClick={handleSignUp} // Sign Up 버튼 클릭 시 핸들러 호출
-                disabled={
-                  !isEmailValid ||
-                  !isNicknameValid ||
-                  confirmPasswordError ||
-                  !isCertificationCodeValid
-                } // 버튼 비활성화 조건
+                onClick={handleSignUp}
+                disabled={!isSignUpEnabled}
+                style={{
+                  backgroundColor: isSignUpEnabled ? "#212529" : "#A0A5AB",
+                }}
               >
                 Sign Up
               </button>
@@ -389,26 +394,26 @@ function SignPage(): JSX.Element {
 
 const Container = styled.div`
   width: 100%;
-  height: 100%; /* 전체 화면 높이 설정 */
+  height: 100%;
   display: flex;
   justify-content: center;
-  align-items: center; /* 수직 중앙 정렬 */
+  align-items: center;
   background-color: #b6c0d3;
 `;
 
 const SignForm = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start; /* 상단 정렬 */
-  align-items: flex-start; /* 왼쪽 정렬 */
+  justify-content: flex-start;
+  align-items: flex-start;
   width: 100%;
-  max-width: 500px; /* 최대 너비 설정 */
-  min-height: 500px; /* 최소 높이 설정 */
+  max-width: 500px;
+  min-height: 500px;
   border: solid #ccd1d9 1px;
   border-radius: 15px;
   background-color: #ffffff;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  padding: 20px; /* 내부 여백 추가 */
+  padding: 20px;
   margin-top: 5%;
   margin-bottom: 20%;
 `;
@@ -417,8 +422,8 @@ const Title = styled.h1`
   width: 100%;
   font-size: 2.5rem;
   font-weight: 1000;
-  text-align: left; /* 왼쪽 정렬 */
-  margin-bottom: 20px; /* 제목 아래 여백 */
+  text-align: left;
+  margin-bottom: 20px;
   color: #333333;
   padding: 20px;
 `;
