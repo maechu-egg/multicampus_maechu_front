07140eccdfbad7dda5727d51da1d00a4df1a9522
leeyeonju02 <85239317+leeyeonju02@users.noreply.github.com>
2024-11-23 22:28:45 +0900

    feat: 버전 명시

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/",
+    "/api",
     createProxyMiddleware({
       target: "https://port-0-workspace-m3sh9tqyf4462309.sel4.cloudtype.app", // 클라우드 타입의 백엔드 주소
       changeOrigin: true,
