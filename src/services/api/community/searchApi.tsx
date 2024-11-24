b579bf631556e908bef12b4f413cdd3f60a41068
leeyeonju02 <85239317+leeyeonju02@users.noreply.github.com>
2024-11-24 18:31:38 +0900

    fix: backend url 수정

--- src/services/api/community/searchApi.tsx
+++ src/services/api/community/searchApi.tsx
@@ -1,7 +1,5 @@
-import axios from 'axios';
-import { Post } from '../../../hooks/community/usePost';
-
-const BASE_URL = 'http://localhost:8001/community/posts';
+import api from "../axios";
+import { Post } from "../../../hooks/community/usePost";
 
 interface SearchResponse {
   posts: Post[];
@@ -15,29 +13,46 @@ interface KeywordSearchResponse {
 
 export const searchApi = {
   // 검색어로 검색
-  searchPosts: async (searchTerm: string, page: number, size: number, token: string) => {
-    return await axios.get<SearchResponse>(`${BASE_URL}/search/${searchTerm}`, {
-      params: { 
-        page, 
-        size 
-      },
-      headers: { 
-        Authorization: `Bearer ${token}` 
+  searchPosts: async (
+    searchTerm: string,
+    page: number,
+    size: number,
+    token: string
+  ) => {
+    return await api.get<SearchResponse>(
+      `/community/posts/search/${searchTerm}`,
+      {
+        params: {
+          page,
+          size,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
-    });
+    );
   },
 
   // 키워드로 검색
-  searchByKeyword: async (keyword: string, page: number, size: number, post_up_sport: string, token: string) => {
-    return await axios.get<KeywordSearchResponse>(`${BASE_URL}/sport/${keyword}`, {
-      params: { 
-        page, 
-        size, 
-        post_up_sport 
-      },
-      headers: { 
-        Authorization: `Bearer ${token}` 
+  searchByKeyword: async (
+    keyword: string,
+    page: number,
+    size: number,
+    post_up_sport: string,
+    token: string
+  ) => {
+    return await api.get<KeywordSearchResponse>(
+      `/community/posts/sport/${keyword}`,
+      {
+        params: {
+          page,
+          size,
+          post_up_sport,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
-    });
-  }
+    );
+  },
 };
