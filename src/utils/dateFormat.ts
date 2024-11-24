58763e673b617b67ac2c8ae9242169e8ec0eb724
w00jinLee <leewoojin001121@gmail.com>
2024-11-25 04:23:25 +0900

    fix: 날짜형식추가(년,월,일)

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -11,3 +11,13 @@ export const formatDate = (dateString: string) => {
   
   return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 };
+
+export const formatDateShort = (dateString: string) => {
+  const date = new Date(dateString);
+  
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  
+  return `${year}-${month}-${day}`;
+};
