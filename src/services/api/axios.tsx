41b217fb7865dc23420b52ffe35bd8388c3e5a40
leeyeonju02 <85239317+leeyeonju02@users.noreply.github.com>
2024-11-25 20:49:00 +0900

    fix: 백엔드 axios 수정

--- src/services/api/axios.tsx
+++ src/services/api/axios.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: process.env.REACT_APP_BACKEND_URL,
-  baseURL: "http://localhost:8080/",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 export default api;
