ee655a0fd397ef6fb2846fb5c8561eeea155a2d7
w00jinLee <leewoojin001121@gmail.com>
2024-11-28 14:10:22 +0900

    fix:식단페이지 스크롤탑수정

--- src/pages/recode/RecordPage.tsx
+++ src/pages/recode/RecordPage.tsx
@@ -61,6 +61,13 @@ function RecordPage(): JSX.Element {
   const closeLoginWarning = () => {
     setIsLoginWarningOpen(false);
   };
+  const handleNavigate = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
 
   // 현재 날짜 기준 월 데이터 조회
   const fetchMonthData = async (date: Date) => {
@@ -257,16 +264,20 @@ function RecordPage(): JSX.Element {
                 <ButtonGroup>
                 {(exerciseDates.includes(selectedDate) || selectedDate === new Date().toISOString().split('T')[0]) && (
                   <ModalButton
-                    onClick={() =>
-                      navigate(`/record/exercise/${selectedDate}`)
-                    }
+                    onClick={() =>{
+                      handleNavigate();
+                      navigate(`/record/exercise/${selectedDate}`);
+                    }}
                   >
                     💪🏻 운동
                   </ModalButton>
                 )}
                 {(dietDates.includes(selectedDate) || selectedDate === new Date().toISOString().split('T')[0]) && (
                   <ModalButton
-                    onClick={() => navigate(`/record/diet/${selectedDate}`)}
+                    onClick={() => {
+                      handleNavigate();
+                      navigate(`/record/diet/${selectedDate}`);
+                    }}
                   >
                     🥗 식단
                   </ModalButton>
